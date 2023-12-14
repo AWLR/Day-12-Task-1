a115aeba1d51447bc1b5353298c709c3a9082ba2
AWLR <lehrelex@gmail.com>
2023-12-14 20:24:39 +0800

    Upload all files

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <div>
+  <h1>Todo app</h1>
+  <input placeholder="Add todo" @keyup.enter ="addTodo" v-model="newTodo"/>
+  <button @click="addTodo()">Add</button>
+  <ul>
+    <li v-for="(todo, index) in todos" :key="index">
+      {{ todo }}
+      <button @click="removeTodo(index)">Remove</button>
+    </li>
+  </ul>
+</div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+    data(){
+      return{
+        newTodo: "",
+        todos: [],
+      }
+    },
+    methods: {
+        addTodo(){
+            if(this.newTodo.trim() !== ''){
+              this.todos.push(this.newTodo.trim());
+              this.newTodo= '';
+            }
+        },
+        removeTodo(index){
+          this.todos.splice(index, 1)
+        }
+    }
+  }
+
+</script>
+
+<style>
+#app{
+  text-align: center;
+  margin-top: 60px;
+}
+input{
+  padding: 10px;
+  font-size: 16px;
+  margin-bottom: 10px;
+}
+ul{
+  list-style-type: none;
+}
+li{
+  margin: 5px;
+}
+
+button{
+  margin-left: 10px;
+  color: green;
+  background-color: aquamarine;
+}
+</style>
